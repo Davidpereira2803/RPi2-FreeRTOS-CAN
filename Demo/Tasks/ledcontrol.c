4f86f1b7d01983ee5c43f25fd7b108d977d4db34
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-07-30 15:58:57 +0200

    Add ledcontrol files

---
+++ Demo/Tasks/ledcontrol.c
@@ -0,0 +1,31 @@
+
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "ledcontrol.h"
+
+#define Base_GPIO 0x3F200000 	// Base address for GPIO
+#define High_GPIO *(gpio + 7)  	// GPIO pin high
+#define Low_GPIO *(gpio + 10) 	// GPIO pin low
+
+volatile unsigned int *gpio = (unsigned int *)Base_GPIO;
+
+void vLEDON(void *pvParameters){
+	uint32_t gpio_pin = *((uint32_t *)pvParameters);
+	// Set GPIO pin to output
+    	*(gpio + ((gpio_pin) / 10)) &= ~(7 << ((gpio_pin % 10) * 3));
+    	*(gpio + ((gpio_pin) / 10)) |= (1 << ((gpio_pin % 10) * 3));
+    	
+    	High_GPIO = 1 << gpio_pin;
+}
+
+void vLEDOFF(void *pvParameters){
+	uint32_t gpio_pin = *((uint32_t *)pvParameters);
+	// Set GPIO pin to output
+    	*(gpio + ((gpio_pin) / 10)) &= ~(7 << ((gpio_pin % 10) * 3));
+    	*(gpio + ((gpio_pin) / 10)) |= (1 << ((gpio_pin % 10) * 3));
+    	
+    	Low_GPIO = 1 << gpio_pin;
+}
+
---
+++ Demo/Tasks/ledcontrol.h
@@ -0,0 +1,9 @@
+#ifndef LEDCONTROL_H
+#define LEDCONTROL_H
+
+void vLEDON(void *pvParameters);
+
+void vLEDOFF(void *pvParameters);
+
+#endif 
+
