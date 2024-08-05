70c7c45b925c3a4a237366e1ecc05bd7b0e27655
DavidPereira2803 <david.pereira.002@student.uni.lu>
2024-08-05 21:08:52 +0200

    remove unnecessery code

--- Demo/main.c
+++ Demo/main.c
@@ -4,14 +4,8 @@
 #include "Drivers/rpi_gpio.h"
 #include "Drivers/rpi_irq.h"
 
-#include "Tasks/ledcontrol.h"
+//#include "Tasks/ledcontrol.h"
 #include "Tasks/Bench/epic/epic.h"
-
-#include <unistd.h>
-#include <time.h>
-
-#define GPIO_PIN_26 26
-#define GPIO_PIN_18 18 
 
 const TickType_t xDelay = 500 * portTICK_PERIOD_MS;
 
@@ -36,31 +30,6 @@ void task2(void *pParam) {
 	}
 }
 
-uint32_t gpio_pin_26 = GPIO_PIN_26;
-uint32_t gpio_pin_18 = GPIO_PIN_18;	
-
-
-
-void tobinary(){
-    // Assuming a 32-bit integer
-    int bits = sizeof(int) * 8;
-
-    for (int i = bits - 1; i >= 0; i--) {
-        // Check if the ith bit is set
-        int bit = (10 >> i) & 1;
-        if(bit == 0){
-           vLEDON(&gpio_pin_26);
-
-        }
-        else{
-          vLEDON(&gpio_pin_18);
-
-        }
-    }
-
-}
-
-
 /**
  *	This is the systems main entry, some call it a boot thread.
  *
@@ -68,21 +37,19 @@ void tobinary(){
  *	-- the same prototype as you'd see in a linux program.
  **/
 int main(void) {
-	rpi_cpu_irq_disable();
+	//rpi_cpu_irq_disable();
 
-	rpi_gpio_sel_fun(47, 1);			
-	rpi_gpio_sel_fun(35, 1);
+	//rpi_gpio_sel_fun(47, 1);			
+	//rpi_gpio_sel_fun(35, 1);
 	
 		
-	tobinary();
+	//tobinary();
 	//xTaskCreate(task1, "LED_0", 128, NULL, 0, NULL);
 	//xTaskCreate(task2, "LED_1", 128, NULL, 0, NULL);
 
 	//xTaskCreate(vLEDON, "LEDyellow", 128, &gpio_pin_26, 1, NULL);
 	//xTaskCreate(vLEDON, "LEDred", 128, &gpio_pin_18, 1, NULL);
 	//xTaskCreate(epic_main_run, "EPICBench", 1024, NULL, 0, NULL);
-
-	//xTaskCreate(tobinary, "binary", 1024, NULL, 0, NULL);
 
 	vTaskStartScheduler();
 
